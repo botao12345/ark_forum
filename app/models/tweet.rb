7f993cd94fa21c951b1f3c229e32d482ec8b5fc7
Tao Bo <botao12345@gmail.com>
2015-12-05 11:16:03 -0500

    removed message validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,8 +2,7 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   has_many :responses
   mount_uploader :photo, PictureUploader
-  validates :message, presence: true
   validates :title, presence: true
-  validates :message, length: {maximum: 140,
+  validates :message, length: {maximum: 1400,
                                too_long: "Message is too long."}
 end
